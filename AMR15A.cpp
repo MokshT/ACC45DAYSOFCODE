c8323ac713b2878f8ac0322ca84509eb74726328
MokshT <93966283+MokshT@users.noreply.github.com>
2022-10-31 20:51:21 +0530

    Add files via upload

---
+++ AMR15A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a,i,y=0,z=0;
+    cin >> n;
+    for (i=0;i<n;i++)
+    {
+        cin >>a;
+        if (a%2==0)
+        y=y+1;
+        else
+        z=z+1;
+    }
+    if (y>z)
+    {
+        cout << "Ready for Battle";
+    }
+    else
+    {
+        cout << "Not Ready";
+    }
+	return 0;
+}
